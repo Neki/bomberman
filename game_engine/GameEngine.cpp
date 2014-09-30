0a5322bb9f7c566df4d0fe08aef43fe1a4bd92e1
Alizee <hellas90@gmail.com>
2014-09-30 14:55:54 +0200

    second commit

---
+++ game_engine/GameEngine.cpp
@@ -0,0 +1,25 @@
+#include "GameEngine.h"
+#include "Entity.h"
+#include "Character.h"
+#include "Bomb.h"
+#include "Block.h"
+#include "Wall.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct World{
+    QInt width;
+    QInt height;
+} World;
+
+
+void main(){
+
+}
+
+void Game::Start(void){
+
+}
