b2d53683f42138663c845cd676bca49bb5d4df75
Benoît Faucon <faucon.benoit@cegetel.net>
2014-10-10 11:12:40 +0200

    Add a GameTimer class to provide reference timestamps during a game.

---
+++ src/common/GameTimer.h
@@ -0,0 +1,30 @@
+#ifndef SRC_COMMON_GAMETIMER_H_
+#define SRC_COMMON_GAMETIMER_H_
+
+#include <QTime>
+
+namespace common {
+
+/**
+ * A unique source of timestamps for a running game.
+ * Note: if the system clock settings are changed during the game, this class
+ * may return invalid results.
+ * Assignment and copy constructors are private as to prevent inadvertently
+ * using several timestamp references for the same game.
+ */
+class GameTimer {
+
+  public:
+    void StartGame();
+    quint32 GetTimestamp();
+
+  private:
+    QTime timer_;
+    GameTimer operator=(const GameTimer& other);
+    GameTimer(const GameTimer& other);
+
+};
+
+}
+
+#endif
