c5155a9681f5f3738e701da07c851faa236716a4
vidalieh <hervevidalie@gmail.com>
2014-10-14 13:37:45 +0200

    Move files.

--- src/common/GameEngine.h
+++ src/common/GameEngine.h
@@ -3,19 +3,40 @@
 
 #include <QObject>
 #include <QPoint>
+#include <QtCore>
+#include "Event.h"
+#include "MoveEvent.h"
+#include "BombEvent.h"
+#include "World.h"
 
 namespace common {
 
-class GameEngine : public QObject {
-  Q_OBJECT
-  public:
-    
-    void AddFireFromAtoB(QPoint a, QPoint b);
-    
-  private:
 
-};
+  class GameEngine : public QObject {
+    Q_OBJECT
+
+    public:
+		GameEngine(Vector<Player> players, QTime max_Duration_, QTime sudden_Death_Mode_Duration_);
+    void AddFireFromAtoB(QPoint a, QPoint b);
+    void startGame();
+    void updateGame();
+    void validateActions();
+    void applyActions();
+    void simulateActions();
+    void sendGameState();
+    void moveCharacter();
+
+    private:
+		QTime maxDuration;
+		Players;
+        World world;
+		
+		void update(int t); // t in ms
+
+  };
+
 
 }
+
 
 #endif /* end of include guard: SRC_SERVER_GAMEENGINE_H_ */
