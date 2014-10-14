4c56ac977ff72b29717ed8db75a1accc1826a169
vidalieh <hervevidalie@gmail.com>
2014-10-14 17:14:11 +0200

    Continue implementation. Not finished.

--- src/common/GameEngine.cpp
+++ src/common/GameEngine.cpp
@@ -1,31 +1,93 @@
-#include "World.h"
+
 #include "GameEngine.h"
-#include "Entity.h"
-#include "Character.h"
-#include "Bomb.h"
-#include "Block.h"
-#include "Wall.h"
 
-#include <iostream>
-#include <string>
 
-void GameEngine::startGame():World{
+
+GameEngine::GameEngine()
+{
+  world_ = new World(80, 40);
+
 }
 
-void GameEngine::updateGame(){
+void GameEngine::startGame() {
+
+  while (true) {
+    update(t)
+  }
+
 }
 
-void GameEngine::validateActions(){
+
+bool addEntity(Entity* entity){
+  if(world_->isWalkable(entity->GetPosition()))
+  {
+    world_->addEntiry(entity);
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
-void GameEngine::applyActions(){
+std::weak_ptr<World> GameEngine::GetWorld() const {
+  return weak_ptr<World> world_;
 }
 
-void GameEngine::simulateActions(){
+void GameEngine::update(int t) {
+  challengeStrategies();
+  simulate(t);
+  sendGameState();
+}
+    
+void GameEngine::challengeStrategies() {
+  for(auto strategy : strategy_ ) {
+    for (auto e : strategy->GetPendingEvents()) {
+      e.visit(this);
+    }
+  }
 }
 
-void GameEngine::sendGameState(){
+void GameEngine::simulate(int t) {
+  for(auto character : world_->GetCharacters) {
+    character->update(t);
+  }
+
+  for(auto column : world_->GetEntities) {
+    for(auto block : column) {
+      for(auto entity : block) {
+        entity->update(t);
+      }
+    }
+  }
+  
+  // TODO : handle fire
+  
+  // TODO : remove destroyed entities
+  
 }
 
-void GameEngine::moveCharacter(){
+void GameEngine::sendGameState() {
+  // TODO
+}
+
+bool GameEngine::accept(ClientEvent<BombEvent>& client_event) {
+  int id = client_event.getClientId();
+  // TODO : Check if value is ok
+  
+  BombEvent e = client_event.getEventData();
+  // TODO : checks on the character
+  //addEntity(Bomb(...));
+  
+}
+
+bool GameEngine::accept(ClientEvent<MoveEvent>& client_event) {
+  int id = client_event.getClientId();
+  // TODO : Check if value is ok
+  
+  MoveEvent e = client_event.getEventData();
+  //moveCharacter(id,);
+}
+
+void GameEngine::moveCharacter(int player_id, QPoint target) {
+
 }
--- src/common/GameEngine.h
+++ src/common/GameEngine.h
@@ -7,33 +7,49 @@
 #include "Event.h"
 #include "MoveEvent.h"
 #include "BombEvent.h"
+#include "BombEvent.h"
+#include "MoveEvent.h"
 #include "World.h"
+#include "Strategy.h"
+#include "Player.h"
 
 namespace common {
 
 
-  class GameEngine : public QObject {
-    Q_OBJECT
+class GameEngine : public QObject {
+  Q_OBJECT
 
-    public:
-		GameEngine(Vector<Player> players, QTime max_Duration_, QTime sudden_Death_Mode_Duration_);
-    void AddFireFromAtoB(QPoint a, QPoint b);
+  public:
+    GameEngine();
+    GameEngine(Vector<Player> players);
+
     void startGame();
-    void updateGame();
-    void validateActions();
-    void applyActions();
-    void simulateActions();
-    void sendGameState();
-    void moveCharacter();
-
-    private:
-		QTime maxDuration;
-		Players;
-        World world;
+    bool addEntity(Entity* entity);
+    std::weak_ptr<World> GetWorld() const;
+    
+    void AddFireFromAtoB(QPoint a, QPoint b);
+    
+  private:
 		
 		void update(int t); // t in ms
-
-  };
+  
+    QTime maxDuration;
+    
+    void update(int t); // t in ms. t is the duration of the frame
+    
+    void challengeStrategies();
+    void simulate(int t); // t in ms. t is the duration of the frame
+    void sendGameState();
+    
+    bool accept(BombEvent& e);
+    bool accept(MoveEvent& e);
+    
+    void moveCharacter(int player_id, QPoint target);
+    
+    std::shared_ptr<World> world_;
+    std::unique_ptr<Strategy> strategies_;
+    
+};
 
 
 }
