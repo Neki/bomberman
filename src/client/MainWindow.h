a8880ea7506d26121a6f7a23da12ca4c78910ae8
hellas90@gmail.com <hellas90@gmail.com>
2014-10-15 22:19:41 +0200

    fix for travis

--- src/client/MainWindow.h
+++ src/client/MainWindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "ui_mainwindow.h"
 #include <memory>
 #include <QMainWindow>
 #include <QTimer>
--- src/client/main.cc
+++ src/client/main.cc
@@ -1,39 +1,13 @@
-#include <iostream>
-#include <QApplication>
-#include <QPushButton>
-#include "src/common/Game.h"
+#include <QtWidgets/QApplication>
+#include "MainWindow.h"
 
-#include "logging.h"
-_INITIALIZE_EASYLOGGINGPP
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	MainWindow w;
 
-int main(int argc, char *argv[]) {
-    std::cout << "Welcome to the client interface of Bomberman :)" << std::endl;
+	w.setWindowTitle("Bomberman");
+	w.show();
 
-    initialize_logger();
-
-    _START_EASYLOGGINGPP(argc, argv);
-    LOG(INFO) << "Started client";
-
-    // This part should be moved to another class, that's just for test
-    QApplication app(argc, argv);
-    QWidget mainWindow;
-    mainWindow.setFixedSize(300, 300);
-
-    QPushButton createGameButton("Create game", &mainWindow);
-    createGameButton.setCursor(Qt::PointingHandCursor);
-    createGameButton.move(100, 75);
-
-    QPushButton joinGameButton("Join game", &mainWindow);
-    joinGameButton.setCursor(Qt::PointingHandCursor);
-    joinGameButton.move(100, 150);
-
-    QPushButton quitWindowButton("Quit Bomberman", &mainWindow);
-    quitWindowButton.setCursor(Qt::PointingHandCursor);
-    quitWindowButton.move(100, 225);
-
-    QObject::connect(&quitWindowButton, SIGNAL(clicked()), &app, SLOT(quit()));
-
-    mainWindow.show();
-
-    return app.exec();
+	return a.exec();
 }
