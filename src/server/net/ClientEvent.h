9ef89100c727b8ee433ed6615ab865e9b388c821
Benoît Faucon <faucon.benoit@cegetel.net>
2014-10-19 17:52:04 +0200

    Move the implementation of the class template ClientEvent to the header.
    Search "Why can templates only be implemented in the header file?" for
    the reason.
    
    Also, now uses the Client class.

--- src/server/net/ClientEvent.h
+++ src/server/net/ClientEvent.h
@@ -2,6 +2,7 @@
 #define SRC_SERVER_NET_CLIENTEVENT_H_
 
 #include <qglobal.h>
+#include "Client.h"
 
 namespace net {
 
@@ -15,14 +16,24 @@ namespace net {
 
     public:
       // TODO(faucon_b): Not sure about having to pass the event by copy
-      ClientEvent(T event, quint8 client_id, quint64 server_timestamp);
-      T getEventData() const;
-      quint8 getClientId() const;
+      ClientEvent(T event, Client client, quint32 server_timestamp)
+        : event_(event),
+          client_(client),
+          server_timestamp_(server_timestamp) {
+      }
+
+      T getEventData() const {
+        return event_;
+      }
+
+      quint8 getClientId() const {
+        return client_.GetId();
+      }
 
     private:
       T event_;
-      quint64 server_timestamp_;
-      quint8 client_id_;
+      Client client_;
+      quint32 server_timestamp_;
 
   };
 
