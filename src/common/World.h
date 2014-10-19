0714f6e2cebde3f98ebd029243d272fc6c743604
vidalieh <hervevidalie@gmail.com>
2014-10-19 22:35:44 +0200

    Declare World's methods.

--- src/common/World.h
+++ src/common/World.h
@@ -1,21 +1,37 @@
 #ifndef SRC_SERVER_WORLD_H_
 #define SRC_SERVER_WORLD_H_
 
-#include "GameEngine.h"
 #include <memory>
+#include <QPoint>
+#include "src/common/entity/Entity.h"
+#include "src/common/entity/Character.h"
+#include "GameEngine.h"
 
 namespace common {
 
+typedef std::vector<std::vector<std::vector<std::shared_ptr<entity::Entity> > > > grid_t;
+
 class World {
   public:
-    World(int width, int height);
+    World(std::weak_ptr<GameEngine> game_engine, int width, int height);
 
     std::weak_ptr<GameEngine> GetGameEngine() const;
+
+    int GetWidth() const;
+    int GetHeight() const;
+    bool CheckCoord(QPoint p) const;
+    bool IsWalkable(QPoint p) const; // Returns true if a character can walk on this case
+    bool StopsFire(QPoint p) const; // Returns true if the case stops fire propagation
+    bool AddItem(std::unique_ptr<entity::Entity> entity); // Adds an entity to the grid. Returns false if it fail.
+    bool AddCharacter(std::unique_ptr<entity::Character> character); // Adds a character to characters_. Returns false if it fail.
+    void removeEntities(); // Removes entities that should be removed
     
   private:
     std::weak_ptr<GameEngine> game_engine_;
-    int width_;
+    int width_; // Width of the world eg number of blocks
     int height_;
+    grid_t entities_;
+    std::vector<std::shared_ptr<entity::Character> > characters_;
 };
 
 }
