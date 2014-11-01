82190f449379ce61b87f7ac9acc98f9af3f035bd
vidalieh <hervevidalie@gmail.com>
2014-11-01 09:55:15 +0100

    Add test 'AddItem'.

--- tests/common/world_test.cc
+++ tests/common/world_test.cc
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "src/common/World.h"
+#include "src/common/entity/Wall.h"
 #include <QPoint>
 
 TEST(World, World) {
@@ -19,4 +20,14 @@ TEST(World, CheckCoord) {
   EXPECT_EQ(world.CheckCoord(QPoint(-1, 0)), false);
   EXPECT_EQ(world.CheckCoord(QPoint(30, 2)), false);
   EXPECT_EQ(world.CheckCoord(QPoint(14, 10)), false);
+}
+
+TEST(World, AddItem) {
+  common::World world(30,10);
+  QPoint pos(4, 4);
+  world.AddItem(std::unique_ptr<common::entity::Wall>( new common::entity::Wall(pos)));
+  EXPECT_EQ(world.IsWalkable(pos), false);
+  EXPECT_EQ(world.StopsFire(pos), true);
+  EXPECT_EQ(world.IsWalkable(QPoint(3, 3)), true);
+  EXPECT_EQ(world.StopsFire(QPoint(3, 3)), false);
 }
