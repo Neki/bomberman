862885e01bded05625ae7f2f4b6fa93dc5769ed1
vidalieh <hervevidalie@gmail.com>
2014-11-03 13:27:41 +0100

    Add map generation in constructor.

--- src/common/World.cc
+++ src/common/World.cc
@@ -1,5 +1,7 @@
 
 #include "World.h"
+#include "entity/Wall.h"
+#include "entity/Block.h"
 #include <cassert>
 
 namespace common {
@@ -14,6 +16,25 @@ World::World(int width, int height)
     entities_.push_back(std::vector<std::vector<std::unique_ptr<entity::Entity> > >());
     for (int y = 0; y < height_; y++) {
       entities_[x].push_back(std::vector<std::unique_ptr<entity::Entity> >());
+      QPoint pos(x, y);
+      if (x % 2 == 1 && y % 2 == 1) {
+        entities_[x][y].emplace_back(std::unique_ptr<entity::Wall>(new entity::Wall(pos)));
+      } else if (x == width_ - 1 && width_ % 2 == 1) { // if width_ is odd
+        entities_[x][y].emplace_back(std::unique_ptr<entity::Wall>(new entity::Wall(pos)));
+      } else if (x == height_ - 1 && height_ % 2 == 1) { // if height_ is odd
+        entities_[x][y].emplace_back(std::unique_ptr<entity::Wall>(new entity::Wall(pos)));
+      } else if ((pos - QPoint(0, 0)).manhattanLength() <= 1 ) { // room for player
+        
+      } else if ((pos - QPoint(0, height_ - (height_%2))).manhattanLength() <= 1) { // room for player
+
+      } else if ((pos - QPoint(width_ - (width_%2), 0)).manhattanLength() <= 1) { // room for player
+
+      } else if ((pos - QPoint(width_ - (width_%2), height_ - (height_%2))).manhattanLength() <= 1) { // room for player
+
+      }
+      else {
+        entities_[x][y].emplace_back(std::unique_ptr<entity::Block>(new entity::Block(pos)));
+      }
     }
   }
   
