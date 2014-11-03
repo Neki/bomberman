36e28d8ecc55732a9ee1d9dd312e072239c17b89
Benoît Faucon <faucon.benoit@cegetel.net>
2014-11-03 20:35:19 +0100

    Add the no_download option to the build process.

--- easyloggingcpp.cmake
+++ easyloggingcpp.cmake
@@ -5,15 +5,26 @@ include(ExternalProject)
 set(EASYLOGGINGCPP_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/easylogging")
 set(EASYLOGGINGCPP_VERSION "9.75")
 
-ExternalProject_Add(easyloggingcpp
-  PREFIX ${EASYLOGGINGCPP_PREFIX}
-  URL "https://github.com/easylogging/easyloggingpp/releases/download/v${EASYLOGGINGCPP_VERSION}/easyloggingpp_v${EASYLOGGINGCPP_VERSION}.tar.gz"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  LOG_DOWNLOAD ON
-  LOG_CONFIGURE OFF
-  LOG_BUILD OFF)
+if(NOT no_download)
+  ExternalProject_Add(easyloggingcpp
+    PREFIX ${EASYLOGGINGCPP_PREFIX}
+    URL "https://github.com/easylogging/easyloggingpp/releases/download/v${EASYLOGGINGCPP_VERSION}/easyloggingpp_v${EASYLOGGINGCPP_VERSION}.tar.gz"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE OFF
+    LOG_BUILD OFF)
+else()
+  ExternalProject_Add(easyloggingcpp
+    PREFIX ${EASYLOGGINGCPP_PREFIX}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE OFF
+    LOG_BUILD OFF)
+endif()
 
 include_directories("${EASYLOGGINGCPP_PREFIX}/src/easyloggingcpp")
 
