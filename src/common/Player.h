f46f994ac9122a27e6a7956b778fe2fb78b6172c
vidalieh <hervevidalie@gmail.com>
2014-11-05 00:06:16 +0100

    Add attribute strategy_.

--- src/common/Player.h
+++ src/common/Player.h
@@ -2,22 +2,26 @@
 #define SRC_COMMON_PLAYER_H_
 
 #include <QObject>
+#include <memory>
+#include "Strategy.h"
 
 namespace common {
     class Player {
     public:
         Player(int id);
         Player(QString name);
-        int id();
-        QString name();
-        int score();
+        int GetId() const;
+        QString GetName() const;
+        int GetScore() const;
         int AddScore(int score_to_add);
         int SubstractScore(int score_to_substract);
+        Strategy* GetStrategy();
 
     protected:
         int id_;
         QString name_;
         int score_;
+        std::unique_ptr<Strategy> strategy_;
     };
 }
 #endif
