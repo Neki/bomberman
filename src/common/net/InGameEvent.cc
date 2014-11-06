107e24e70fd65dc92b0f85f50949354a8369bfce
vidalieh <hervevidalie@gmail.com>
2014-11-06 22:43:42 +0100

    Add method GetCharacterId

--- src/common/net/InGameEvent.cc
+++ src/common/net/InGameEvent.cc
@@ -15,6 +15,10 @@ InGameEvent::InGameEvent(quint8 character_id, quint64 timestamp)
 
 InGameEvent::~InGameEvent() {}
 
+quint8 InGameEvent::GetCharacterId() const {
+  return character_id_;
+}
+
 void InGameEvent::SerializeBaseInGameEvent(QDataStream& stream, EventId event_id) const {
   Event::SerializeBaseEvent(stream, event_id);
   stream << character_id_;
