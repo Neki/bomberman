503307ff4baa705cb0136dcf8b0c3c7d2dbbd730
vidalieh <hervevidalie@gmail.com>
2014-11-10 20:27:46 +0100

    Add constructor.

--- src/common/Game.h
+++ src/common/Game.h
@@ -9,12 +9,13 @@ namespace common {
 
 class Game {
   public:
+    Game();
     std::weak_ptr<Player> GetPlayer(int id);
     int GetPlayersCount();
     void AddPlayer(std::shared_ptr<Player> player);
     /* Each player should have an unique ID ! */
-    void Start();
-    void End();
+    virtual void Start() = 0;
+    virtual void End() = 0;
   private:
     std::vector<std::shared_ptr<Player> > players_;
 
