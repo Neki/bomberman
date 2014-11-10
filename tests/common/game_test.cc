044e45be3ab1b4f3c0ae5ddb3f3e2e0debe6b616
vidalieh <hervevidalie@gmail.com>
2014-11-10 22:35:49 +0100

    Update test.

--- tests/common/game_test.cc
+++ tests/common/game_test.cc
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
 #include "src/common/Game.h"
+#include <memory>
+#include <QString>
 
 TEST(Game, AddPlayer) {
     common::Game game;
-    common::Player player("SpiderMan");
+    std::shared_ptr<common::Player> player (new common::Player(1, "SpiderMan"));
     EXPECT_EQ(game.GetPlayersCount(), 0);
     game.AddPlayer(player);
     EXPECT_EQ(game.GetPlayersCount(), 1);
@@ -11,11 +13,9 @@ TEST(Game, AddPlayer) {
 
 TEST(Game, GetPlayer) {
     common::Game game;
-    common::Player player("IronMan");
-    int player_id = game.AddPlayer(player);
-
-    common::Player *new_player = game.GetPlayer(player_id);
-    EXPECT_EQ(new_player->GetName(), player.GetName());
-
-    EXPECT_THROW(game.GetPlayer(40), std::exception);
+    std::shared_ptr<common::Player> player(new common::Player(2, "IronMan"));
+    game.AddPlayer(player);
+    std::weak_ptr<common::Player> get_player = game.GetPlayer(2);
+    std::shared_ptr<common::Player> new_player = get_player.lock();
+    EXPECT_EQ(new_player->GetName(), player->GetName());
 }
--- tests/common/player_test.cc
+++ tests/common/player_test.cc
@@ -2,7 +2,7 @@
 #include "src/common/Player.h"
 
 TEST(Player, SubstractScore) {
-    common::Player player("SpiderMan");
+    common::Player player(1, "SpiderMan");
     player.SubstractScore(3);
     EXPECT_EQ(player.GetScore(), -3);
 }
