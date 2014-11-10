fee38392a5782f2ff79bad0504e4b55f42efcbb5
vidalieh <hervevidalie@gmail.com>
2014-11-10 19:48:53 +0100

    Correct indentation.

--- src/common/Game.cc
+++ src/common/Game.cc
@@ -4,30 +4,30 @@
 #include <stdexcept>
 
 namespace common {
-    int Game::AddPlayer(Player& player) {
-        int player_id = GetPlayersCount();
-        players_.insert(std::pair<int, Player*>(player_id, &player));
+int Game::AddPlayer(Player& player) {
+  int player_id = GetPlayersCount();
+  players_.insert(std::pair<int, Player*>(player_id, &player));
 
-        return player_id;
-    }
+  return player_id;
+}
 
-    Player* Game::GetPlayer(int id) {
-        if (!players_.count(id)) {
-            throw std::out_of_range("Unknown player id");
-        }
+Player* Game::GetPlayer(int id) {
+  if (!players_.count(id)) {
+    throw std::out_of_range("Unknown player id");
+  }
 
-        return players_[id];
-    }
+  return players_[id];
+}
 
-    int Game::GetPlayersCount() {
-        return (int) players_.size();
-    }
+int Game::GetPlayersCount() {
+  return (int) players_.size();
+}
 
-    void Game::Start() {
-        // start the game !
-    }
+void Game::Start() {
+  // start the game !
+}
 
-    void Game::End() {
-        // end the game !
-    }
+void Game::End() {
+  // end the game !
+}
 }
